30da407f694813539aa09347fdb42b80498a93b7
Rogelio Zarate <i.am@rogeliozarate.com>
2013-08-16 20:28:32 -0500

    Extend example
    - Query every image for aditional info

--- app.rb
+++ app.rb
@@ -7,19 +7,23 @@ CONFIG_FILE = "config.yaml"
 
   if File.exist?(CONFIG_FILE) && File.ftype(CONFIG_FILE) == 'file'
     CONFIG = YAML::load(File.read('config.yaml'))
-    puts 'config file loaded'
+    puts 'Config file loaded'
+    puts '----------------------------------------------------------'
   else
     abort "\n\nFile config.yaml does not exist. Please edit config.example and save it as config.yaml\n\n"
   end
-  puts 
-  puts 
 
-
-  FlickRaw.api_key= CONFIG['auth']['api_key']
-  FlickRaw.shared_secret=CONFIG['auth']['consumer_secret']
+  FlickRaw.api_key        = CONFIG['auth']['api_key']
+  FlickRaw.shared_secret  = CONFIG['auth']['consumer_secret']
 
   list   = flickr.photos.getRecent
+  
   list.each do |image|
+    secret  = image.secret
+    id      = image.id
+    info    = flickr.photos.getInfo :photo_id => id, :secret => secret
     puts image.id
-    puts image.title
+    puts info.title
+    puts info.dates.taken 
   end
+  
